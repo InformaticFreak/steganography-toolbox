91fe6afb5684c607c6439e5d1009a280fc84dff7
FabianBartl <fabian@informatic-freak.de>
2022-11-14 17:35:25 +0100

    merge hideBitsInImage into hideFileInImage

--- src/tools.py
+++ src/tools.py
@@ -13,36 +13,37 @@ from functions import *
 hide functions
 """
 
-def hideBitsInImage(image:Image.Image, bits:bitarray, *, repeat:bool=True, pos:int="least", progressBar:tqdm=True, **kwargs_pbar) -> Image.Image:
+def hideFileInImage(inputImagePath:str, outputImagePath:str, inputFilePath:str, *, repeat:bool=True, pos:int="least", **kwargs_save) -> bool:
 	# check types
-	if type(image) is not Image.Image:
-		raise TypeError(f"image={type(image)} must be of type Image.Image")
-	if type(bits) is not bitarray:
-		raise TypeError(f"bits={type(bits)} must be of type bitarray")
+	if type(inputImagePath) is not str:
+		raise TypeError(f"inputImagePath={type(inputImagePath)} must be of type str")
+	if type(outputImagePath) is not str:
+		raise TypeError(f"outputImagePath={type(outputImagePath)} must be of type str")
+	if type(inputFilePath) is not str:
+		raise TypeError(f"inputFilePath={type(inputFilePath)} must be of type str")
 	if type(repeat) is not bool:
 		raise TypeError(f"repeat={type(repeat)} must be of type bool")
 	if type(pos) not in (int, str):
 		raise TypeError(f"pos={type(pos)} must be of type int as specific str")
-	if type(progressBar) not in (tqdm, bool):
-		raise TypeError(f"progressBar={type(progressBar)} must be of type tqdm or bool")
-	# convert image to array
-	width, height = image.size
-	pixels = image2Array(image)
-	# set progress bar behavior
-	if type(progressBar) is bool:
-		if progressBar:
-			# setup progress bar
-			pbar = tqdm(total=width*height, unit="px", **kwargs_pbar)
-			# progress bar updater functions
-			pbarUpdate = lambda x=1: pbar.update(x)
-		else:
-			pbarUpdate = lambda _=_: _
-	elif type(progressBar) is tqdm:
-		pbarUpdate = lambda x=1: progressBar.update(x)
+	# load image to array
+	inputImage = loadImage(inputImagePath)
+	width, height = inputImage.size
+	pixels = image2Array(inputImage)
+	# convert file to bitarray
+	bits = file2BitArray(inputFilePath)
+	# setup progress bar
+	pbar = tqdm(
+		total = width * height,
+		unit = "px",
+		desc = "Progress",
+		unit_scale = True,
+		colour = "#ffb900"
+	)
+	pbarUpdate = lambda x=1: pbar.update(x)
 	# get bits lenght
 	bitsLen = len(bits)
 	bitsInd = 0
-	# hide file in image
+	# hide file in input image
 	BREAK = False
 	for y in range(height):
 		for x in range(width):
@@ -62,32 +63,10 @@ def hideBitsInImage(image:Image.Image, bits:bitarray, *, repeat:bool=True, pos:i
 		if BREAK:
 			break
 	# close progress bar
-	if type(progressBar) is bool and progressBar:
-		pbar.colour = "#8ce10b"
-		pbar.close()
-	# return array as image
-	return array2Image(pixels)
-
-def hideFileInImage(inputImagePath:str, outputImagePath:str, inputFilePath:str, *, repeat:bool=True, pos:int="least", **kwargs_save) -> bool:
-	# check types
-	if type(inputImagePath) is not str:
-		raise TypeError(f"inputImagePath={type(inputImagePath)} must be of type str")
-	if type(outputImagePath) is not str:
-		raise TypeError(f"outputImagePath={type(outputImagePath)} must be of type str")
-	if type(inputFilePath) is not str:
-		raise TypeError(f"inputFilePath={type(inputFilePath)} must be of type str")
-	if type(repeat) is not bool:
-		raise TypeError(f"repeat={type(repeat)} must be of type bool")
-	if type(pos) not in (int, str):
-		raise TypeError(f"pos={type(pos)} must be of type int as specific str")
-	# load image
-	inputImage = loadImage(inputImagePath)
-	width, height = inputImage.size
-	# convert file to bitarray
-	bits = file2BitArray(inputFilePath)
-	# hide bitarray in image
-	outputImage = hideBitsInImage(inputImage, bits, repeat=repeat, pos=pos, desc="Progress", unit_scale=True, colour="#ffb900")
+	pbar.colour = "#8ce10b"
+	pbar.close()
 	# save image
+	outputImage = array2Image(pixels)
 	error = saveImage(outputImagePath, outputImage, **kwargs_save)
 	# return if saving was successful
 	return not error
