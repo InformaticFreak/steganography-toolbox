52ef02c932607722d89bff10b18cfab36bdff89d
FabianBartl <fabian@informatic-freak.de>
2022-11-14 17:43:24 +0100

    merge seekBitsInImage into seekFileInImage

--- src/tools.py
+++ src/tools.py
@@ -75,62 +75,6 @@ def hideFileInImage(inputImagePath:str, outputImagePath:str, inputFilePath:str, 
 seek functions
 """
 
-def seekBitsInImage(image:Image.Image, *, pos:int="least", lenght:int=None, progressBar:tqdm=True, **kwargs_pbar) -> bitarray:
-	# check types
-	if type(image) is not Image.Image:
-		raise TypeError(f"image={type(image)} must be of type Image.Image")
-	if type(lenght) not in (int, type(None)):
-		raise TypeError(f"lenght={type(lenght)} must be of type int or None")
-	if type(progressBar) not in (tqdm, bool):
-		raise TypeError(f"progressBar={type(progressBar)} must be of type tqdm or bool")
-	# load image to array
-	width, height = image.size
-	pixels = image2Array(image)
-	# check values
-	if not 0 <= lenght <= width*height:
-		raise ValueError(f"{lenght=} must be between 0 and {width*height} (width x height of image)")
-	# set lenght to pixel count if not set
-	if lenght is None:
-		lenght = width * height
-	# set progress bar behavior
-	if type(progressBar) is bool:
-		if progressBar:
-			# setup progress bar
-			pbar = tqdm(total=width*height, unit="px", **kwargs_pbar)
-			# progress bar updater functions
-			pbarUpdate = lambda x=1: pbar.update(x)
-		else:
-			pbarUpdate = lambda _=_: _ 
-	elif type(progressBar) is tqdm:
-		pbarUpdate = lambda x=1: progressBar.update(x)
-	# seek bits from image
-	bits = bitarray()
-	bitsInd = 0
-	BREAK = False
-	for y in range(height):
-		for x in range(width):
-			# break if end of bits reached and repeat is False
-			if bitsInd >= lenght:
-				pbar.total = bitsInd // 3
-				BREAK = True
-				break
-			# get least significant bits
-			pixel = pixels[y][x]
-			bits.append(getBit(pixel[0], pos=pos))
-			bits.append(getBit(pixel[1], pos=pos))
-			bits.append(getBit(pixel[2], pos=pos))
-			pbarUpdate()
-			bitsInd += 1
-		# break if inner loop breaks
-		if BREAK:
-			break
-	# close progress bar
-	if type(progressBar) is bool and progressBar:
-		pbar.colour = "#8ce10b"
-		pbar.close()
-	# return bits as bitarray
-	return bits
-
 def seekFileInImage(inputImagePath:str, outputFilePath:str, *, pos:int="least", lenght:int=None) -> bool:
 	# check types
 	if type(inputImagePath) is not str:
@@ -144,11 +88,47 @@ def seekFileInImage(inputImagePath:str, outputFilePath:str, *, pos:int="least", 
 	# load image
 	inputImage = loadImage(inputImagePath)
 	width, height = inputImage.size
-	# set lenght to pixel count if not set
-	if lenght is None:
+	pixels = image2Array(image)
+	# check values
+	if lenght is not None:
+		if not (0 <= lenght <= width*height):
+			raise ValueError(f"{lenght=} must be between 0 and {width*height=}")
+	else:
 		lenght = width * height
-	# seek bitarray from image and save in file
-	bits = seekBitsInImage(inputImage, pos=pos, lenght=lenght, desc="Progress", unit_scale=True, colour="#ffb900")
+	# setup progress bar
+	pbar = tqdm(
+		total = width * height,
+		unit = "px",
+		desc = "Progress",
+		unit_scale = True,
+		colour = "#ffb900"
+	)
+	pbarUpdate = lambda x=1: pbar.update(x)
+	# seek bits from input image
+	bits = bitarray()
+	bitsInd = 0
+	BREAK = False
+	for y in range(height):
+		for x in range(width):
+			# break if end of bits reached and repeat is False
+			if bitsInd >= lenght:
+				pbar.total = bitsInd // 3
+				BREAK = True
+				break
+			# get bit at given position
+			pixel = pixels[y][x]
+			bits.append(getBit(pixel[0], pos=pos))
+			bits.append(getBit(pixel[1], pos=pos))
+			bits.append(getBit(pixel[2], pos=pos))
+			pbarUpdate()
+			bitsInd += 1
+		# break if inner loop breaks
+		if BREAK:
+			break
+	# close progress bar
+	pbar.colour = "#8ce10b"
+	pbar.close()
+	# save seeked bits
 	error = bitArray2File(outputFilePath, bits)
 	# return if saving was successful
 	return not error
